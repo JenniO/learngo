b4825a9cecca98d105336741db237746f3be5ab0
Jennifer Ozolins <j.ozolins@28apps.de>
2019-08-26 16:06:11 +0200

    Variable execises

---
+++ 06-variables/01-basic-data-types/exercises/01-print-the-literals/main.go
@@ -0,0 +1,33 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Print the literals
+//
+//  1. Print a few integer literals
+//
+//  2. Print a few float literals
+//
+//  3. Print true and false bool literals
+//
+//  4. Print your name using a string literal
+//
+//  5. Print a non-english sentence using a string literal
+//
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Println(1, 7, 13, -23, 42)
+	fmt.Println(.23, 4.2, 123.456)
+	fmt.Println(true, false)
+	fmt.Println("Jenni")
+	fmt.Println("Merhaba, adım Jenni!")
+}
